f187be20d7c8bf7b3201adbf3a086f1a8405d53d
Lela Deslauriers <lela.deslauriers@gmail.com>
2023-01-04 22:53:55 -0800

    commit tree e2543716a9589e5e0fa97afbbc05b90232d5cedc

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  let reversedWord = reverseString(word);
+
+  return reversedWord === word;
 }
 
 /* 
   Add your pseudocode here
+    reverse the input string
+
+    if the reversed string is the same as the input
+      return true
+    else
+      return false
 */
 
 /*
   Add written explanation of your solution here
+  Create 2 functions, one of them a helper function with the job of reversing the string with method chaining and returning it. 
+  The main function assigns a variable to the output of the helper function. 
+  Then we use the === operator to return true or false
 */
 
 // You can run `node index.js` to view these console logs
